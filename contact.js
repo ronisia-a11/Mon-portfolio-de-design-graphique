c885ff57dbf379ac8fd7009ba5889101c8a1fc12
Ronisia <offilieoffilie@gmail.com>
2026-07-01 13:22:40 +0100

    Mise a jour du site. Remplacement de la page menu par index et mise a jour dans les autres pages

--- contact.js
+++ contact.js
@@ -1,9 +1,20 @@
 document.getElementById("contactForm").addEventListener("submit", function(e) {
   e.preventDefault(); // Empêche l'envoi réel
 
-  // Efface le formulaire
-  this.reset();
+  // Récupération des données
+  const nom = event.target.nom.value;
+  const telephone = event.target.telephone.value;
+  const email = event.target.email.value;
+  const projet = event.target.projet.value;
+  const ville = event.target.ville.value;
+  const description = event.target.description.value;
 
-  // Affiche un petit message
-  document.getElementById("message").textContent = "✅ Le formulaire a bien été envoyé !";
+  // Affiche un message de confirmation
+  alert("Merci " + nom + "! Votre demande a été envoyée.\n" +
+        "Téléphone: " + telephone + "\n" +
+        "Email: " + email + "\n" +
+        "Projet: " + projet + "\n" +
+        "Ville: " + ville + "\n" +
+        "Description: " + description);
+        
 });
